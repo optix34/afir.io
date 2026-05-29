5defb982af5165895f85a2051ad885eed5f53212
evg <optix500@gmail.com>
2026-05-29 18:22:10 +0300

    Delete oklik_clone directory

--- oklik_clone/app/store/Tasks.js
+++
@@ -1,8 +0,0 @@
-Ext.define('Store.oklik_clone.store.Tasks', {
-    extend: 'Ext.data.Store',
-    model: 'Store.oklik_clone.model.Task',
-    autoLoad: true,
-    pageSize: 25,
-    remoteSort: true,
-    remoteFilter: true
-});
