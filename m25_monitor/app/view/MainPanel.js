9da609e7bde4099ea2d9e9c986b4059a436c55b4
evg <optix500@gmail.com>
2026-05-29 21:23:24 +0300

    Update MainPanel.js

--- m25_monitor/app/view/MainPanel.js
+++ m25_monitor/app/view/MainPanel.js
@@ -1,5 +1,9 @@
 /**
- * MainPanel.js – правая панель с iframe и панелью инструментов.
+ * MainPanel.js — правая панель (основная область) для M25 Monitor.
+ * Содержит iframe для отображения внешней страницы и тулбар с кнопками управления.
+ *
+ * @class Store.m25_monitor.view.MainPanel
+ * @extends Ext.panel.Panel
  */
 Ext.define('Store.m25_monitor.view.MainPanel', {
     extend: 'Ext.panel.Panel',
@@ -7,6 +11,24 @@ Ext.define('Store.m25_monitor.view.MainPanel', {
 
     layout: 'fit',
     title: l('M25 Monitor — внешняя страница'),
+
+    /**
+     * Текущий URL, загруженный в iframe
+     * @property {String} currentIframeSrc
+     */
+    currentIframeSrc: 'about:blank',
+
+    /**
+     * Ссылка на iframe-компонент
+     * @property {Ext.Component} iframe
+     */
+    iframe: null,
+
+    /**
+     * Ссылка на тулбар
+     * @property {Ext.toolbar.Toolbar} toolbar
+     */
+    toolbar: null,
 
     initComponent: function() {
         this.createIframe();
@@ -16,22 +38,33 @@ Ext.define('Store.m25_monitor.view.MainPanel', {
         this.callParent(arguments);
     },
 
+    /**
+     * Создаёт iframe-компонент с методом getIframeDom для доступа к DOM-элементу
+     */
     createIframe: function() {
         this.iframe = Ext.create('Ext.Component', {
             autoEl: {
                 tag: 'iframe',
-                src: 'about:blank',
+                src: this.currentIframeSrc,
                 style: 'width: 100%; height: 100%; border: none;'
             },
+            /**
+             * Возвращает DOM-элемент iframe
+             * @return {HTMLIFrameElement}
+             */
             getIframeDom: function() {
-                return this.getEl().dom;
+                var el = this.getEl();
+                return el ? el.dom : null;
             }
         });
-        this.currentIframeSrc = 'about:blank';
     },
 
+    /**
+     * Создаёт тулбар с кнопками "Обновить", "Открыть в новом окне" и информационной строкой
+     */
     createToolbar: function() {
         var me = this;
+
         this.toolbar = Ext.create('Ext.toolbar.Toolbar', {
             dock: 'top',
             items: [
@@ -39,9 +72,15 @@ Ext.define('Store.m25_monitor.view.MainPanel', {
                     text: l('Обновить'),
                     iconCls: 'fa fa-refresh',
                     handler: function() {
-                        var iframeEl = me.iframe.getIframeDom();
-                        if (iframeEl) iframeEl.src = me.currentIframeSrc;
-                    }
+                        var iframeDom = me.iframe.getIframeDom();
+                        if (iframeDom && me.currentIframeSrc && me.currentIframeSrc !== 'about:blank') {
+                            // Принудительная перезагрузка iframe
+                            iframeDom.src = me.currentIframeSrc;
+                        } else {
+                            Ext.Msg.alert(l('Информация'), l('Нет загруженной страницы для обновления.'));
+                        }
+                    },
+                    scope: me
                 },
                 {
                     text: l('Открыть в новом окне'),
@@ -50,29 +89,66 @@ Ext.define('Store.m25_monitor.view.MainPanel', {
                         if (me.currentIframeSrc && me.currentIframeSrc !== 'about:blank') {
                             window.open(me.currentIframeSrc, '_blank');
                         } else {
-                            Ext.Msg.alert(l('Информация'), l('Сначала выберите объект.'));
+                            Ext.Msg.alert(l('Информация'), l('Сначала выберите объект в левой панели.'));
                         }
-                    }
+                    },
+                    scope: me
                 },
-                '->',
+                '->', // Растягивающийся разделитель
                 {
                     xtype: 'component',
-                    html: '<span style="color:#888;">' + l('Выберите объект в левой панели') + '</span>',
+                    html: '<span style="color:#64748b;">' + l('Выберите объект в левой панели') + '</span>',
                     itemId: 'infoText'
                 }
             ]
         });
     },
 
+    /**
+     * Загружает указанный URL в iframe и обновляет информационную строку.
+     * Вызывается из Navigation.js при выборе транспортного средства.
+     * @param {String} url - URL для загрузки
+     * @param {String} vehicleName - название транспортного средства (опционально)
+     */
     loadUrl: function(url, vehicleName) {
-        var iframeDom = this.iframe.getIframeDom();
+        var me = this;
+        if (!url) {
+            Ext.log.warn('[M25] loadUrl called without URL');
+            return;
+        }
+
+        var iframeDom = me.iframe.getIframeDom();
         if (iframeDom) {
             iframeDom.src = url;
-            this.currentIframeSrc = url;
+            me.currentIframeSrc = url;
+            Ext.log('[M25] Loaded URL in iframe: ' + url);
+        } else {
+            Ext.log.error('[M25] Cannot get iframe DOM element');
+            return;
+        }
+
+        // Обновляем информационную строку в тулбаре
+        var infoText = me.down('#infoText');
+        if (infoText && vehicleName) {
+            var safeName = Ext.String.htmlEncode(vehicleName);
+            infoText.update('<span style="color:#2563eb;">' + l('Текущий объект: ') + safeName + '</span>');
+        } else if (infoText && !vehicleName) {
+            infoText.update('<span style="color:#64748b;">' + l('Выберите объект в левой панели') + '</span>');
+        }
+    },
+
+    /**
+     * Сброс iframe (загружает about:blank) и очищает информационную строку
+     */
+    reset: function() {
+        var iframeDom = this.iframe.getIframeDom();
+        if (iframeDom) {
+            iframeDom.src = 'about:blank';
+            this.currentIframeSrc = 'about:blank';
         }
         var infoText = this.down('#infoText');
-        if (infoText && vehicleName) {
-            infoText.update('<span style="color:#2563eb;">' + l('Текущий объект: ') + Ext.String.htmlEncode(vehicleName) + '</span>');
+        if (infoText) {
+            infoText.update('<span style="color:#64748b;">' + l('Выберите объект в левой панели') + '</span>');
         }
     }
 });
